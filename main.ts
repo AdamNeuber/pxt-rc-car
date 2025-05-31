6f4cabdde9452c98dbddc380e1108ba677edcc88
Adam Neuber <adam.neuber.024@pslib.cz>
2025-05-31 09:04:37 +0200

    minor changes

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 radio.setGroup(50)
 radio.setTransmitPower(7)
 
-let strafeToggle:string = "false"
+let strafeToggle: string = "false"
 
 let slowR = 0
 let slowL = 0
 
-let pitch = 0
-let roll = 0
+let pitch
+let roll
+let sendMsg = pitch + "," + roll + "," + slowR + "," + slowL + "," + strafeToggle
 
-input.onButtonPressed(Button.A, function() {
-    if(slowL == 0){
+input.onButtonPressed(Button.A, function () {
+    if (slowL == 0) {
         slowL = 1
     } else {
         slowL = 0
@@ -25,12 +26,11 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 
-
-input.onLogoEvent(TouchButtonEvent.Pressed, function() {
-    if(strafeToggle == "false"){
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (strafeToggle == "false") {
         strafeToggle = "true"
-    }else{
-        strafeToggle =  "false"
+    } else {
+        strafeToggle = "false"
     }
 })
 
@@ -40,13 +40,15 @@ input.onButtonPressed(Button.AB, function () {
     slowL = 0
     slowR = 0
     strafeToggle = "false"
-    radio.sendString(pitch + "," + roll + "," + slowR + "," + slowL + "," + strafeToggle)
+    radio.sendString(sendMsg)
 })
 
-basic.forever(function() {
+
+
+basic.forever(function () {
     pitch = - Math.round(input.acceleration(Dimension.Y)) / 10
     roll = Math.round(input.acceleration(Dimension.X)) / 10
 
-    radio.sendString(pitch + "," + roll + "," + slowR + "," + slowL + "," + strafeToggle)
+    radio.sendString(sendMsg)
     basic.pause(50)
 })
--- rc.ts
+++ rc.ts
@@ -1,34 +1,42 @@
 radio.setGroup(50)
 radio.setTransmitPower(7)
 
-function rcTurn(strenght: number, state: string) {
+let msg
+let carPitch
+let carRoll
+let carSlowL
+let carSlowR
+let carStrafe
+
+
+function turn(strength: number, state: string) {
     if (state === "true") {
-        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + strenght)
-        PCAmotor.Servo(PCAmotor.Servos.S2, 90 + strenght)
-        PCAmotor.Servo(PCAmotor.Servos.S3, 90 - strenght)
-        PCAmotor.Servo(PCAmotor.Servos.S4, 90 - strenght)
-    }else {
-        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + strenght)
-        PCAmotor.Servo(PCAmotor.Servos.S2, 90 + strenght)
-        PCAmotor.Servo(PCAmotor.Servos.S3, 90 + strenght)
-        PCAmotor.Servo(PCAmotor.Servos.S4, 90 + strenght)
+        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + strength)
+        PCAmotor.Servo(PCAmotor.Servos.S2, 90 + strength)
+        PCAmotor.Servo(PCAmotor.Servos.S3, 90 - strength)
+        PCAmotor.Servo(PCAmotor.Servos.S4, 90 - strength)
+    } else {
+        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + strength)
+        PCAmotor.Servo(PCAmotor.Servos.S2, 90 + strength)
+        PCAmotor.Servo(PCAmotor.Servos.S3, 90 + strength)
+        PCAmotor.Servo(PCAmotor.Servos.S4, 90 + strength)
     }
 }
 
-radio.onReceivedString(function(receivedString: string) {
-    let msg = receivedString.split(",")
-    let pitch = Math.clamp(-90, 90, parseInt(msg[0]))
-    let roll = Math.clamp(-90, 90,parseInt(msg[1]))
-    let slowL = parseInt(msg[2])
-    let slowR = parseInt(msg[3])
-    let strafeToggle = msg[4]
-    
-    if (pitch != 0) {
-        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + pitch)
-        PCAmotor.Servo(PCAmotor.Servos.S2, 100 - pitch)
-        PCAmotor.Servo(PCAmotor.Servos.S3, 90 + pitch)
-        PCAmotor.Servo(PCAmotor.Servos.S4, 100 - pitch)
-        
+radio.onReceivedString(function (receivedString: string) {
+    msg = receivedString.split(",")
+    carPitch = Math.clamp(-90, 90, parseInt(msg[0]))
+    carRoll = Math.clamp(-90, 90, parseInt(msg[1]))
+    carSlowL = parseInt(msg[2])
+    carSlowR = parseInt(msg[3])
+    carStrafe = msg[4]
+
+    if (carPitch != 0) {
+        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + carPitch)
+        PCAmotor.Servo(PCAmotor.Servos.S2, 100 - carPitch)
+        PCAmotor.Servo(PCAmotor.Servos.S3, 90 + carPitch)
+        PCAmotor.Servo(PCAmotor.Servos.S4, 100 - carPitch)
+
     } else {
         PCAmotor.Servo(PCAmotor.Servos.S1, 90)
         PCAmotor.Servo(PCAmotor.Servos.S2, 90)
@@ -36,21 +44,17 @@ radio.onReceivedString(function(receivedString: string) {
         PCAmotor.Servo(PCAmotor.Servos.S4, 90)
     }
 
-    if (roll < -20 || roll > 20) {
-        rcTurn(roll,strafeToggle)
+    if (carRoll < -20 || carRoll > 20) {
+        turn(carRoll, carStrafe)
     }
 
-    while(slowR == 1) {
-        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + pitch/2)
-        PCAmotor.Servo(PCAmotor.Servos.S3, 90 + pitch/2)
-        slowL = 0
-        break
+    if (carSlowR == 1 && carSlowL == 0) {
+        PCAmotor.Servo(PCAmotor.Servos.S1, 90 + carPitch / 2)
+        PCAmotor.Servo(PCAmotor.Servos.S3, 90 + carPitch / 2)
     }
 
-    while (slowL == 1) {
-        PCAmotor.Servo(PCAmotor.Servos.S2, 90 - pitch/2)
-        PCAmotor.Servo(PCAmotor.Servos.S4, 90 - pitch/2)
-        slowR = 0
-        break
+    if (carSlowL == 1 && carSlowR == 0) {
+        PCAmotor.Servo(PCAmotor.Servos.S2, 90 - carPitch / 2)
+        PCAmotor.Servo(PCAmotor.Servos.S4, 90 - carPitch / 2)
     }
 })
